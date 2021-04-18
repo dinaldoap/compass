eb13c128e4dbfaec09d38818d52f4215c22ffa9f
dinaldoap <dinaldoap@gmail.com>
2021-04-18 13:11:46 +0000

    feat: run Price before Change

--- compass/transact/deposit.py
+++ compass/transact/deposit.py
@@ -4,7 +4,7 @@ from compass.step import Actual, Change, Price, Quantity, Target
 
 class Deposit(Transact):
     def run(self):
-        steps = [Target(), Actual(), Change(), Price(), Quantity()]
+        steps = [Target(), Actual(), Price(), Change(), Quantity()]
         data = None
         for step in steps:
             data = step.run(data=data)
