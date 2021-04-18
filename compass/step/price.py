7065fb6ed25faa27fc0a70dfc52ffb35c49f4820
dinaldoap <dinaldoap@gmail.com>
2021-04-18 13:27:13 +0000

    feat: read price from file

--- compass/step/price.py
+++ compass/step/price.py
@@ -5,4 +5,10 @@ import pandas as pd
 
 class Price(Step):
     def run(self, data: pd.DataFrame):
-        raise NotImplementedError()
+        output = pd.read_excel('data/price.xlsx')
+        output = output[['Ticker', 'Price']]
+        output = data.join(output.set_index('Ticker'),
+                           on='Ticker', how='inner')
+        assert len(data) == len(output), 'output\'s length ({}) is expected to be the same as input\'s ({}).'.format(
+            len(output), len(data))
+        return output
