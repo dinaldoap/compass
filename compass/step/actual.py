2d5c9138933a8ab414311593405f37e764a501f1
dinaldoap <dinaldoap@gmail.com>
2021-04-18 13:11:00 +0000

    feat: read actual allocation from file

--- compass/step/actual.py
+++ compass/step/actual.py
@@ -5,4 +5,7 @@ import pandas as pd
 
 class Actual(Step):
     def run(self, data: pd.DataFrame):
-        raise NotImplementedError()
+        actual = pd.read_excel('data/actual.xlsx')
+        actual = actual[['Ticker', 'Actual']]
+        result = data.join(actual.set_index('Ticker'), how='inner')
+        return result
