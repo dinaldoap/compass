7ff0ab0d58bc77e88fa8ffb576027a099c9d5fd8
dinaldoap <dinaldoap@gmail.com>
2021-04-18 13:30:00 +0000

    feat: add assertion and rename output variable

--- compass/step/actual.py
+++ compass/step/actual.py
@@ -5,7 +5,10 @@ import pandas as pd
 
 class Actual(Step):
     def run(self, data: pd.DataFrame):
-        actual = pd.read_excel('data/actual.xlsx')
-        actual = actual[['Ticker', 'Actual']]
-        result = data.join(actual.set_index('Ticker'), how='inner')
-        return result
+        output = pd.read_excel('data/actual.xlsx')
+        output = output[['Ticker', 'Actual']]
+        output = data.join(output.set_index('Ticker'),
+                           on='Ticker', how='inner')
+        assert len(data) == len(output), 'output\'s length ({}) is expected to be the same as input\'s ({}).'.format(
+            len(output), len(data))
+        return output
