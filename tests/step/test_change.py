4f17c71e06bacd0bf74a045cc8653d8682b386cb
dinaldoap <dinaldoap@gmail.com>
2021-04-24 16:30:25 +0000

    test: test rounding

--- tests/step/test_change.py
+++ tests/step/test_change.py
@@ -1,18 +1,24 @@
+from pandas.io import pytables
 from compass.step import Change
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
+import pytest
 
-def test_first_deposit():
+@pytest.mark.parametrize("value, change", 
+                        [(  10., [2, 4]), # exact change
+                         (  11., [2, 4]), # round below x.5
+                         (   9., [1, 3])]) # round above x.5
+def test_first_deposit(value, change):
     data = {
         'Target': [.2, .8],
         'Actual': [0, 0],
         'Price': [1., 2.],
     }
     input = pd.DataFrame(data)
-    output = Change(10.).run(input)
+    output = Change(value).run(input)
     data.update({
-        'Change': [2, 4]
+        'Change': change
     })
     expected = pd.DataFrame(data)
     assert_frame_equal(expected, output)
