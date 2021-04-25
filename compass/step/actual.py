ef9e9a2dd900662c7d4de252f5412b1bcb348f94
dinaldoap <dinaldoap@gmail.com>
2021-04-25 13:24:29 +0000

    feat: integrate with actual portfolio from CEI

--- compass/step/actual.py
+++ compass/step/actual.py
@@ -1,11 +1,16 @@
 from .base import Step
+from compass.source import CEI
 
 import pandas as pd
 
 
 class Actual(Step):
+    def __init__(self):
+        super().__init__()
+        self.source = CEI('data/actual.xlsx')
+
     def run(self, input: pd.DataFrame):
-        output = pd.read_excel('data/actual.xlsx')
+        output = self.source.read()
         output = output[['Ticker', 'Actual']]
         output = input.join(output.set_index('Ticker'),
                             on='Ticker', how='inner')
