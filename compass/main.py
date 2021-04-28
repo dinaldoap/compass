91ae9340bca44c4786350d95c5071ee06489f5f3
dinaldoap <dinaldoap@gmail.com>
2021-04-28 01:40:12 +0000

    feat: turn directory parameter not required

--- compass/main.py
+++ compass/main.py
@@ -12,7 +12,7 @@ def run(argv):
                     These files can have additional columns beside the specified in the layout.
                 ''')
     parser.add_argument('value', type=float, help='Value of the deposit.')
-    parser.add_argument('-d', '--directory', required=True, type=str,
+    parser.add_argument('-d', '--directory', type=str,
                         help='Directory of the portfolio (default: data).', default='data')
     namespace = parser.parse_args(argv)
     args = dict(vars(namespace))
