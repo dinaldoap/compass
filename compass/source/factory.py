656843475fc20055c1495c14d92483e794e26841
dinaldoap <dinaldoap@gmail.com>
2021-04-28 09:09:05 +0000

    refactor: move factory functions to factory.py

--- compass/source/factory.py
+++ compass/source/factory.py
@@ -1,8 +1,21 @@
 from .base import Source
-from .file import StandardPrice, LayoutError
+from .file import CeiActual, StandardActual, StandardPrice, StandardTarget, LayoutError
 from .http import YahooPrice
 
 from pathlib import Path
+
+
+def create_target(config: dict) -> Source:
+    path = Path(config['directory'], 'target.xlsx')
+    return StandardTarget(path=path)
+
+
+def create_actual(config: dict) -> Source:
+    path = Path(config['directory'], 'actual.xlsx')
+    try:
+        return StandardActual(path=path)
+    except LayoutError:
+        return CeiActual(path=path)
 
 
 def create_price(config: dict) -> Source:
