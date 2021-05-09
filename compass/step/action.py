f3334b7ba6892ae3494ebf0f50292e3d1bcbe3a4
dinaldoap <dinaldoap@gmail.com>
2021-05-09 14:59:04 +0000

    feat: output allocation after change

--- compass/step/action.py
+++ compass/step/action.py
@@ -5,5 +5,10 @@ import pandas as pd
 
 class Action(Step):
     def run(self, input: pd.DataFrame):
-        input.to_excel('data/action.xlsx', index=False)
-        return input
+        output = input.copy()
+        output['After'] = (output['Actual'] +
+                           output['Change']) * output['Price']
+        output['After'] = output['After'] / output['After'].sum()
+        output['After'] = output['After'].round(2)
+        output.to_excel('data/action.xlsx', index=False)
+        return output
