e7f020b74ce2e5f5349ce9068af860039c231fdb
dinaldoap <dinaldoap@gmail.com>
2021-05-13 08:58:24 +0000

    chore: add script to release the package

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pyinstaller compass/main.py --onefile --name compass \
+                                    --exclude-module tkinter \
+                                    --hidden-import='cmath'
+python setup.py bdist_wheel --universal
