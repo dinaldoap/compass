c0b67664f82f1e2cc84d2c53ca0eed4c03931bcd
dinaldoap <dinaldoap@gmail.com>
2021-05-16 15:56:05 +0000

    feat: add packaging to build

--- package.sh
+++ package.sh
@@ -2,5 +2,5 @@
 
 pyinstaller compass/main.py --onefile --name compass \
                                     --exclude-module tkinter \
-                                    --hidden-import='cmath'
-python setup.py bdist_wheel --universal
+                                    --hidden-import cmath
+#python setup.py bdist_wheel --universal
