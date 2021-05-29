1b22f35135e7cb6a8ef90174971dbea179158376
dinaldoap <dinaldoap@gmail.com>
2021-05-29 10:45:54 +0000

    refactor: execute faster packagng first

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+python setup.py bdist_wheel --universal --quiet
 pyinstaller compass/__main__.py --onefile --name compass \
                                     --exclude-module tkinter \
                                     --hidden-import cmath \
                                     --log-level ERROR
-python setup.py bdist_wheel --universal --quiet
