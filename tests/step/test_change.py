269a28b6a7e74b37035e3c67cf86df52a6ae4297
dinaldoap <dinaldoap@gmail.com>
2021-06-03 09:53:52 +0000

    feat: add rebalancing

--- tests/step/test_change.py
+++ tests/step/test_change.py
@@ -40,3 +40,20 @@ def test_withdraw(value, change):
     })
     expected = pd.DataFrame(data)
     assert_frame_equal(expected, output)
+
+@pytest.mark.parametrize("value, change", 
+                        [(  0., [-2, 1]), # exact change
+                        ])
+def test_rebalance(value, change):
+    data = {
+        'Target': [.2, .8],
+        'Actual': [4, 3],
+        'Price': [1., 2.],
+    }
+    input = pd.DataFrame(data)
+    output = Change(value).run(input)
+    data.update({
+        'Change': change
+    })
+    expected = pd.DataFrame(data)
+    assert_frame_equal(expected, output)
