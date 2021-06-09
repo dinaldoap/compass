4842fe49cef8a3a08fb26fd7d1b0b5577f0be332
dinaldoap <dinaldoap@gmail.com>
2021-06-09 09:46:26 +0000

    feat: read price from Warren

--- compass/source/file.py
+++ compass/source/file.py
@@ -1,5 +1,6 @@
 from .base import Source
 
+from babel.numbers import parse_decimal
 from datetime import date
 import pandas as pd
 from pathlib import Path
@@ -88,7 +89,7 @@ class CeiHtmlActual(Source):
 class WarrenHtmlActual(Source):
     '''
     HTML file with data from Home Page -> Trade -> Meus ativos -> QUANTIDADE of Warren (https://warren.com.br/app/#/v3/trade).
-    The columns Cód. de Negociação and Qtde. are, respectivelly, renamed to Ticker and Actual. 
+    The columns are named as Ticker and Actual. 
 
     ...
     '''
@@ -97,12 +98,16 @@ class WarrenHtmlActual(Source):
         self.path = Path(path)
         self.selection_pattern = r'class="selected">\s*QUANTIDADE'
         self.table_pattern = r'QUANTIDADE(.+)Favoritos'
+        self.ticker_pattern = r'(?:%| )(\w+) (\d+) '
+
         _check_pattern_layout(
             self.path, 'https://warren.com.br/app/#/v3/trade', self.selection_pattern)
         _check_last_update(self.path, date)
 
     def read(self) -> pd.DataFrame:
-        data = _parse_html(self.path, self.table_pattern)
+        data = _parse_html(self.path, self.table_pattern,
+                           self.ticker_pattern, 'Actual')
+        data['Actual'] = data['Actual'].astype(int)
         return data
 
 
@@ -119,6 +124,32 @@ class StandardPrice(Source):
 
     def read(self, tickers=None):
         return pd.read_excel(self.path)
+
+
+class WarrenHtmlPrice(Source):
+    '''
+    HTML file with data from Home Page -> Trade -> Meus ativos -> PREÇO ATUAL of Warren (https://warren.com.br/app/#/v3/trade).
+    The columns are named as Ticker and Price. 
+
+    ...
+    '''
+
+    def __init__(self, path: Path, date=date.today()):
+        self.path = Path(path)
+        self.selection_pattern = r'class="selected">\s*PREÇO ATUAL'
+        self.table_pattern = r'PREÇO ATUAL(.+)Favoritos'
+        self.ticker_pattern = r'(?:%| )(\w+) R\$ ([\d\.,]+) '
+
+        _check_pattern_layout(
+            self.path, 'https://warren.com.br/app/#/v3/trade', self.selection_pattern)
+        _check_last_update(self.path, date)
+
+    def read(self, tickers=None) -> pd.DataFrame:
+        data = _parse_html(self.path, self.table_pattern,
+                           self.ticker_pattern, 'Price')
+        data['Price'] = data['Price'].apply(
+            lambda str: float(parse_decimal(str, locale='pt_BR', strict=True)))
+        return data
 
 
 class LayoutError(Exception):
@@ -161,18 +192,16 @@ def _check_last_update(path: Path, expected_date: date) -> None:
             '{} is out of date. Last update is expected to be at {} or later.'.format(path, expected_date))
 
 
-def _parse_html(path: Path, table_pattern):
+def _parse_html(path: Path, table_pattern, ticker_pattern, value_column):
     text = _clean_html(_read_content(path))
     match = re.search(table_pattern, text)
-    assert len(match.groups()) == 1, 'It is expected only one table with quantities embraced by the pattern {} in the cleaned html {}.'.format(
+    assert len(match.groups()) == 1, 'It is expected only one table with values embraced by the pattern {} in the cleaned html {}.'.format(
         table_pattern, text)
     table = match.group(0)
-    ticker_pattern = r'(\w+) (\d+)'
     data = []
     for row in re.findall(ticker_pattern, table):
         data.append(row)
-    data = pd.DataFrame(data, columns=['Ticker', 'Actual'])
-    data['Actual'] = data['Actual'].astype(int)
+    data = pd.DataFrame(data, columns=['Ticker', value_column])
     return data
 
 
