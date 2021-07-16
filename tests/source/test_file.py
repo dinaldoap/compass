40c4a27b1582a1effaf30e1e634e35664c7665b3
dinaldoap <dinaldoap@gmail.com>
2021-07-16 09:33:13 +0000

    refactor: remove CeiActual

--- tests/source/test_file.py
+++ tests/source/test_file.py
@@ -15,15 +15,7 @@ def test_StandardTarget():
     assert_frame_equal(expected, output[['Name', 'Ticker', 'Target']])
 
 def test_StandardActual():
-    output = StandardActual('tests/data/actual_standard.xlsx').read()
-    expected = pd.DataFrame({
-        'Ticker': ['BITO39', 'BIEF39'],
-        'Actual': [1, 2]
-    })
-    assert_frame_equal(expected, output[['Ticker', 'Actual']])
-
-def test_CEIActual():
-    output = CeiActual('tests/data/actual.xlsx').read()
+    output = StandardActual('tests/data/actual.xlsx').read()
     expected = pd.DataFrame({
         'Ticker': ['BITO39', 'BIEF39'],
         'Actual': [1, 2]
