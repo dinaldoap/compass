44cd9ce8986d501138ac3cf5e58e8ec10146c0a0
dinaldoap <dinaldoap@gmail.com>
2021-08-10 17:28:14 +0000

    refactor: split Action into multiple classes

---
+++ compass/step/balance.py
@@ -0,0 +1,20 @@
+from .base import Step
+
+import pandas as pd
+
+
+class Balance(Step):
+
+    def run(self, input: pd.DataFrame):
+        output = input.copy()
+        output['Before'] = output['Actual'] * output['Price']
+        output['Before'] = _to_percentage(output['Before'])
+        output['After'] = (output['Actual'] +
+                           output['Change']) * output['Price']
+        output['After'] = _to_percentage(output['After'])
+        return output
+
+
+def _to_percentage(series: pd.Series):
+    series = series / series.sum()
+    return series.round(2)
--- compass/target/factory.py
+++ compass/target/factory.py
@@ -1,10 +1,8 @@
 from .base import Target
 from .file import Standard
 
-from pathlib import Path
 
-
-def create_action(config: dict) -> Target:
+def create_output(config: dict) -> Target:
     return Standard(path=config['output'])
 
 
