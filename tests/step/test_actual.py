54a3ef886c5d48f37d13c6a30b097c0cce99e080
dinaldoap <dinaldoap@gmail.com>
2021-08-17 13:45:01 +0000

    feat: keep columns when adding changes to actual

--- tests/step/test_actual.py
+++ tests/step/test_actual.py
@@ -1,5 +1,5 @@
 from compass.step import Actual, ActualAddedChange
-from compass.source.file import StandardActual
+from compass.source.file import StandardActual, StandardOutput
 
 import pandas as pd
 from pandas.testing import assert_frame_equal
@@ -24,11 +24,11 @@ def test_actual_added_change():
     input = pd.DataFrame({
         'Ticker': ['BITO39', 'BIEF39'],
         'Actual': [1, 2],
-        'Change': [1, 2],
     })
     expected = pd.DataFrame({
         'Ticker': ['BITO39', 'BIEF39'],
         'Actual': [2, 4]
     })
-    output = ActualAddedChange().run(input)
+    output = ActualAddedChange(source=StandardOutput(
+        'tests/data/output.xlsx')).run(input)
     assert_frame_equal(expected, output[['Ticker', 'Actual']])
