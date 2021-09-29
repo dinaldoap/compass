5b6659e1733f5f4dd39c84eff0d8ddef31f70b1d
dinaldoap <dinaldoap@gmail.com>
2021-09-29 01:12:43 +0000

    feat: parameterize rebalance as bool

--- compass/__main__.py
+++ compass/__main__.py
@@ -21,9 +21,8 @@ def parse_args(argv):
                 ''')
     parser.add_argument('value', type=parse_decimal,
                         help='Value to be deposited (greater than zero) or withdrawed (less than zero). Zero also can be used when only rebalancing is required.')
-    parser.add_argument('-r', '--rebalance',
-                        action='store_true', help='''Allow rebalancing. A rebalancing is done when one distance is exceeded (see --absolute-distance and --relative-distance).
-                                                     A rebalancing moves the portfolio up to the point where the exceeded distance goes back to the allowed range.''')
+    parser.add_argument('-r', '--rebalance', type=parse_bool,
+                        help='''Allow rebalancing. A rebalancing is done when one distance is exceeded (see --absolute-distance and --relative-distance).
     parser.add_argument('-b', '--absolute-distance', type=float,
                         help='Absolute distance allowed between the actual and the target allocation of each ticker and group (default: 5%%).', default=.05)
     parser.add_argument('-l', '--relative-distance', type=float,
