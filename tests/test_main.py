432f35e854cb58d989eb16c4e703953aaba39033
dinaldoap <dinaldoap@gmail.com>
2022-01-20 00:28:42 +0000

    test: cover config file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,19 @@
-from compass.__main__ import parse_args, transaction
+from compass.__main__ import parse_args, transaction, _add_config
 
-import pandas as pd
+import os
 
 _EXPECTED_ARGS = {
+    # command line
     "value": 1000.0,
     "rebalance": True,
     "target": "tests/data/portfolio.xlsx",
     "actual": "tests/data/portfolio.xlsx",
     "price": "tests/data/portfolio.xlsx",
     "output": "data/output.xlsx",
-    "expense_ratio": 0.0003,
-    "spread_ratio": 0.0,
+    # configuration (compass.ini)
+    "expense_ratio": 0.1,
+    "spread_ratio": 0.2,
+    # default
     "absolute_distance": 0.05,
     "relative_distance": 0.25,
 }
@@ -28,7 +31,8 @@ def test_parse_args():
             "tests/data/portfolio.xlsx",
             "--output",
             "data/output.xlsx",
-        ]
+        ],
+        "tests/data/compass.ini",
     )
     assert _EXPECTED_ARGS == args
 
