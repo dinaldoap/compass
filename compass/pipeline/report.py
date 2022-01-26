b9dc22b04e602d0678270573117cc923030ddc00
dinaldoap <dinaldoap@gmail.com>
2022-01-26 11:38:32 +0000

    feat: add report  with changes

--- compass/pipeline/report.py
+++ compass/pipeline/report.py
@@ -1,48 +1,26 @@
 from .base import Pipeline
-import pandas as pd
-from compass.number import parse_decimal
+from compass import source, target
+from compass.step import (
+    ReadSource,
+    Join,
+    WriteTarget,
+)
 
 
 class Report(Pipeline):
-    columns = {
-        "Entrada/Saída": "Transaction",  # Credito == deposit and Debito == withdraw
-        "Data": "Date",
-        "Movimentação": "Type",
-        "Produto": "Ticker",
-        "Quantidade": "Change",
-        "Preço unitário": "Price",
-    }
+    def __init__(self, config):
+        self.config = config
 
     def run(self):
-        df_trans = (
-            pd.read_excel("2022.xlsx")
-            .pipe(print_df)
-            .pipe(lambda df: df[self.columns.keys()])
-            .rename(self.columns, axis="columns")
-            .pipe(lambda df: df[df["Change"].str.fullmatch(r"\d+")])
-            .assign(Change=lambda df: df["Change"].apply(int))
-            .pipe(lambda df: df[df["Price"] != "-"])
-            .query("Type == 'Transferência - Liquidação'")
-            .assign(
-                Change=lambda df: (
-                    (df["Transaction"] == "Credito")
-                    + (df["Transaction"] == "Debito") * -1
-                )
-                * df["Change"]
-            )
-            .assign(Ticker=lambda df: df["Ticker"].apply(split_first_trim))
-            .pipe(lambda df: df[["Date", "Ticker", "Change", "Price"]])
-            .pipe(print_df)
-        )
-
-
-def split_first_trim(text):
-    return text.split("-")[0].strip()
-
-
-def print_df(df):
-    # print(df['Price'].unique())
-    print(df)
-    # print(len(df))
-    # print(df.dtypes)
-    return df
+        steps = [
+            ReadSource(source=source.create_transaction(config=self.config)),
+            Join(
+                source=source.create_target(config=self.config),
+                on="Ticker",
+                how="inner",
+            ),
+            WriteTarget(target=target.create_output(config=self.config)),
+        ]
+        data = None
+        for step in steps:
+            data = step.run(input=data)
