06950cf726b5d8b788e10aed9731706bb303f981
dinaldoap <dinaldoap@gmail.com>
2022-01-27 09:30:04 +0000

    test: multiply values by 100

--- tests/step/test_report.py
+++ tests/step/test_report.py
@@ -16,13 +16,13 @@ def test_change_history():
                 "BIEF39",
             ],
             "Change": [1, 1, -1, 1, 9],
-            "Price": [0.5, 1.5, 2.0, 2.0, 9.0],
+            "Price": [50.0, 150.0, 200.0, 200.0, 90.0],
         }
     )
     expected = (
-        input.assign(AvgPrice=[0.5, 1, 1, 1.5, 9.0])
-        .assign(CapitalGain=[0.0, 0.0, 1.0, 0.0, 0.0])
-        .assign(Taxes=[0.0, 0.0, 0.15, 0.0, 0])
+        input.assign(AvgPrice=[50.0, 100.0, 100.0, 150.0, 90])
+        .assign(CapitalGain=[0.0, 0.0, 100.0, 0.0, 0.0])
+        .assign(Taxes=[0.0, 0.0, 15.0, 0.0, 0.0])
     )
     output = ChangeHistoryReport(tax_rate=0.15).run(input)
     assert_frame_equal(expected, output)
