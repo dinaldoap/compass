f5f4c1c4634b29daba63f68e1fd2ff2eb27b8129
dinaldoap <dinaldoap@gmail.com>
2022-01-27 18:58:46 +0000

    feat: read price from main trade's page

--- compass/source/file.py
+++ compass/source/file.py
@@ -182,8 +182,8 @@ class WarrenHtmlPrice(Source):
 
     def __init__(self, path: Path, date=date.today()):
         self.path = Path(path)
-        self.selection_pattern = r"Todos meus favoritos"
-        self.table_pattern = r"Todos meus favoritos([A-Z\d\$ \+\-,%]+)"
+        self.selection_pattern = r"class=\"selected\">\s*PREÇO ATUAL"
+        self.table_pattern = r"PREÇO ATUAL(.+)Favoritos"
         self.ticker_pattern = r"([A-Z\d]+) R\$ ([\d\.,]+) "
         _check_extension(self.path, "html")
         _check_pattern_layout(self.path, _WARREN_PATTERN, self.selection_pattern)
@@ -288,8 +288,8 @@ def _parse_html(path: Path, table_pattern, ticker_pattern, value_column):
     text = _clean_html(_read_content(path))
     match = re.search(table_pattern, text)
     assert (
-        len(match.groups()) == 1
-    ), "It is expected only one table with values embraced by the pattern {} in the cleaned html {}.".format(
+        match is not None
+    ), "It is expected one table with values embraced by the pattern {} in the cleaned html {}.".format(
         table_pattern, text
     )
     table = match.group(0)
