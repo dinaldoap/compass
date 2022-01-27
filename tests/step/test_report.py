998c17db5b98f66e5eba0d1a62fee0c2f0053079
dinaldoap <dinaldoap@gmail.com>
2022-01-27 08:58:13 +0000

    test: remove unecessary cases

--- tests/step/test_report.py
+++ tests/step/test_report.py
@@ -7,21 +7,18 @@ from pandas.testing import assert_frame_equal
 def test_change_history():
     input = pd.DataFrame(
         {
-            "Date": [f"{i:02}/01/2022" for i in range(1, 9)],
+            "Date": [f"{i:02}/01/2022" for i in range(1, 6)],
             "Ticker": [
                 "BITO39",
                 "BITO39",
                 "BITO39",
                 "BITO39",
                 "BIEF39",
-                "BIEF39",
-                "BIEF39",
-                "BIEF39",
             ],
-            "Change": [1, 1, -1, 1, 1, 1, -1, 1],
-            "Price": [0.5, 1.5, 2.0, 2.0, 1.0, 3.0, 4.0, 4.0],
+            "Change": [1, 1, -1, 1, 9],
+            "Price": [0.5, 1.5, 2.0, 2.0, 9.0],
         }
     )
-    expected = input.assign(AvgPrice=[0.5, 1, 1, 1.5, 1.0, 2.0, 2.0, 3.0])
+    expected = input.assign(AvgPrice=[0.5, 1, 1, 1.5, 9.0])
     output = ChangeHistoryReport().run(input)
     assert_frame_equal(expected, output)
