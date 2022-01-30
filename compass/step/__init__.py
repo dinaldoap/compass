74aac881d4b40ebd20fc3f0449c016abac9dd477
dinaldoap <dinaldoap@gmail.com>
2022-01-30 16:09:05 +0000

    feat: add change history view

--- compass/step/__init__.py
+++ compass/step/__init__.py
@@ -3,6 +3,6 @@ from .actual import Actual
 from .change import Change
 from .price import Price
 from .balance import Balance
-from .print import Print
+from .print import *
 from .report import *
 from .target import Target
