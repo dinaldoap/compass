a3d5df8a76326853b12a81f1bc60cbaa0230d4e9
dinaldoap <dinaldoap@gmail.com>
2022-02-03 08:49:53 +0000

    refactor: rename classes

---
+++ compass/pipeline/change.py
@@ -0,0 +1,38 @@
+from .base import Pipeline
+from compass import source, target, model
+from compass.step import (
+    Actual,
+    Change,
+    Price,
+    Target,
+    AllocationReport,
+    ChangePrint,
+    TransactionPrint,
+)
+
+
+class ChangePosition(Pipeline):
+    def __init__(self, config):
+        self.config = config
+
+    def run(self):
+        steps = [
+            Target(source=source.create_target(config=self.config)),
+            Actual(source=source.create_actual(config=self.config)),
+            Price(source=source.create_price(config=self.config)),
+            Change(
+                value=self.config["value"],
+                rebalance=self.config["rebalance"],
+                absolute_distance=self.config["absolute_distance"],
+                relative_distance=self.config["relative_distance"],
+            ),
+            AllocationReport(),
+            TransactionPrint(
+                rebalance=self.config["rebalance"],
+                calculator=model.create_calculator(config=self.config),
+            ),
+            ChangePrint(target=target.create_output(config=self.config)),
+        ]
+        data = None
+        for step in steps:
+            data = step.run(input=data)
--- compass/step/report.py
+++ compass/step/report.py
@@ -5,7 +5,22 @@ import pandas as pd
 import numpy as np
 
 
-class ChangeReport(Step):
+class AllocationReport(Step):
+    def run(self, input: pd.DataFrame):
+        output = input.copy()
+        output["Before"] = output["Actual"] * output["Price"]
+        output["Before"] = _to_percentage(output["Before"])
+        output["After"] = (output["Actual"] + output["Change"]) * output["Price"]
+        output["After"] = _to_percentage(output["After"])
+        return output
+
+
+def _to_percentage(series: pd.Series):
+    series = series / series.sum()
+    return series.round(2)
+
+
+class TransactionPrint(Step):
     def __init__(self, rebalance: bool, calculator: Calculator):
         self.rebalance = rebalance
         self.calculator = calculator
