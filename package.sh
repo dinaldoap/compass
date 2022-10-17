9fa810890d63ede022383f1898267c6ef4bd8ed5
dinaldoap <dinaldoap@gmail.com>
2022-10-17 18:04:44 +0000

    chore: lock dependencies

--- package.sh
+++ package.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-pip wheel --no-deps --wheel-dir dist .
+mkdir --parents build && \
+cat requirements.lock | grep '^\w' > build/constraints.lock && \
+pip wheel --constraint=build/constraints.lock --no-deps --wheel-dir=dist .
 #pyinstaller compass/__main__.py --onefile --name compass \
 #                                    --exclude-module tkinter \
 #                                    --hidden-import cmath \
