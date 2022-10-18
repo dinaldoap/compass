8d6b5829ea63e4b46f038a4c4e2523716501250c
dinaldoap <dinaldoap@gmail.com>
2022-10-18 01:26:35 +0000

    chore: make packaging for production more restrict

--- package.sh
+++ package.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-mkdir --parents build && \
-cat requirements.lock | grep '^\w' > build/constraints.lock && \
-pip wheel --constraint=build/constraints.lock --no-deps --wheel-dir=dist .
+pip wheel --constraint=requirements-prod.lock --no-deps --wheel-dir=dist .
 #pyinstaller compass/__main__.py --onefile --name compass \
 #                                    --exclude-module tkinter \
 #                                    --hidden-import cmath \
