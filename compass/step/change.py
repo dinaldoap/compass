b831e58716b40412c1de109cef855add4aff8630
dinaldoap <dinaldoap@gmail.com>
2024-02-10 13:40:25 -0300

    refactor: solve warning

--- compass/step/change.py
+++ compass/step/change.py
@@ -125,7 +125,9 @@ def _hierarchy_change(
                 relative_distance,
             )
             # apply _parent_group_change_partial, with freezed arguments, to each DataFrameGroup
-            percentage = group_percentage.apply(_parent_group_change_partial)
+            percentage = group_percentage.apply(
+                _parent_group_change_partial, include_groups=False
+            )
         leaf = percentage["Group"] == percentage[f"Group_{level}"]
         change.loc[leaf] = percentage.loc[leaf, f"Change_{level}"]
     return change
